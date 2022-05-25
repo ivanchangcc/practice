6df14bacae717ab8152315807fb86b924caab07e
Ivan Chang Chun Chieh <ivanchang@Ivans-MacBook-Pro.local>
2022-05-25 13:03:02 +0800

    first change

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,6 +2,6 @@ export const env: string = import.meta.env.VITE_NODE_ENV || "development";
 export const logLevel: string = import.meta.env.VITE_LOG_LEVEL || "info";
 
 export const welcomeBanner: string =
-  import.meta.env.VITE_WELCOME_BANNER || "Welcome Anonymous!";
+  import.meta.env.VITE_WELCOME_BANNER || "Morning Iva!";
 export const backendUrl: string =
   import.meta.env.VITE_BACKEND_URL || "https://dog.ceo/api/breeds/image/random";
